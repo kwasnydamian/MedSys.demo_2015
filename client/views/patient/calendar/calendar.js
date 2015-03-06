bc2ca388779f80d222f501974ad9f04fc3a0bc83
luran990 <luraniec.dorian@gmail.com>
2015-03-06 04:30:10 -0800

    Merge branch 'master' of /Users/dorian/Desktop/MedSys.demo_2015 with conflicts.

---
+++ client/views/patient/calendar/calendar.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Dorian on 05.03.15.
+ */
+SuperCalendar.events.onDayClick = function (event, template) {
+// put your custom code here.
+};
--- server/publications.js
+++ server/publications.js
@@ -1,23 +1,6 @@
 /**
  * Created by damian on 04.03.15.
  */
-if(Meteor.isServer){
-    Meteor.publish('powiaty',function(){
-        return Powiaty.find();
-    });
-    Meteor.publish('przychodnie',function(){
-        return Przychodnie.find({});
-    });
-    Meteor.publish('specjalnosci',function(){
-        return Specjalnosci.find();
-    });
-    Meteor.publish('wojewodztwa',function(){
-        return Wojewodztwa.find();
-    });
-    Meteor.publish('wizyty',function(){
-        return Wizyty.find();
-    });
-    Meteor.publish('uzytkownicy',function(){
-        return Uzytkownicy.find();
-    });
-}
+Meteor.publish('calendar', function () {
+    return Calendar.find();
+});
