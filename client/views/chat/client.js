6b03f0ed1b481aed8fc3da54c0b17c4bb2361101
kwasnydamian <damian.kwasny@onebi.eu>
2015-03-16 08:22:16 +0100

    praca z ankietami pre-screening

--- client/views/chat/client.js
+++ client/views/chat/client.js
@@ -3,10 +3,12 @@
  */
 Template.messages.helpers({
     messages: function() {
-        var doctorId = document.getElementById('lekarze').value;
-        if(doctorId!==0){
-            return Messages.find({id_patient:Meteor.userId(),id_doctor:doctorId}, { sort: { time: 1}});
+        if(Session.get('idLekarza')){
+            return Messages.find({id_patient:Meteor.userId(),id_doctor:Session.get('idLekarza')}, { sort: { time: 1}});
         }
+    },
+    czyAutor: function(messages){
+        return true;
     }
 })
 
@@ -35,3 +37,32 @@ Template.input.events = {
         }
     }
 }
+
+Template.client.helpers({
+    wszystkie:function(){
+        return PollsPatients.find({id_pacjent:Meteor.userId(),id_lekarz:Session.get('idLekarza'),isDone:false},{fields:{id_ankieta:1}});
+    },
+    getPollName:function(id_ankieta){
+        return Polls.find({_id:id_ankieta});
+    },
+    isPoll:function(count){
+        if(count==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+});Template.client.events({
+    'click .pollItem':function(event){
+        var id = event.currentTarget;
+        $("#pollModal").modal("show");
+
+        pollPatient = PollsPatients.find({_id:id});
+        Session.set('idPollPatient',pollPatient._id);
+        Session.set('idPoll',pollPatient.id_ankieta);
+
+        $("#idPollPatient").value=id.id;
+    }
+})
+
